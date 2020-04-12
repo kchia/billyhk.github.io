d742ef10b60a7056d4dd5854c6a681a81cdff109
billyh <billyhkaufman@outlook.com>
2020-04-12 17:46:19 -0400

    finished readME

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-//the following sequences will be compared to judge if the player is correct
+//two arrays will be compared to determine if the player is correct
 let engineSequence = [];
 let playerSequence = [];
 
-//a flash is a move by the engine
+//a "flash" is a move by the engine
 //a turn is the current round of the game, that is how many flashes the engine will play
 let flash;
 let turn;
 
-//engineTurn is used to determine when the engineTurn is over. if the engine has played the same number of flashes as the number of the current turn, then the engineTurn is over, and it is time for the the player to guess
+//engineTurn is used to determine when the engineTurn is over
+//if the engine has played the same number of flashes as the number of the current turn, then the engineTurn is over
 let engineTurn;
 
 //correct is used to determine what will happen after the player guesses
 //if the player is not correct: game over
-//if the player is correct but it's not the last turn: proceed with next turn
-//if the player is correct and it is the last turn: win = true
 let correct;
 
-//intervalId is set to 0 because it will be updated in functions below
+//intervalId is set to 0 because it will be updated by certain functions
 let intervalId = 0;
 
-//on is set to false so we can still press the lights but nothing happens
+//on is set to false when the player does not need to interact with the game board(i.e. when it's the engine's turn, during win game and game over)
 let on = false;
 
 //win is set default to false because it will later be used to trigger for the winGame function
 let win = false;
 
+//turn counter is used as the game display. it shows the current turn, game over, and game win
 const turnCounter = document.querySelector('.turn-counter');
 
+//each light will need its own event handler
 const topLeft = document.querySelector('#topLeft');
 const topRight = document.querySelector('#topRight');
 const bottomRight = document.querySelector('#bottomRight');
 const bottomLeft = document.querySelector('#bottomLeft');
 
-//reset starts hidden
+//reset button starts hidden
 const startButton = document.querySelector('.start-button');
 const resetButton = document.querySelector('.reset-button');
 resetButton.classList.toggle('hidden'); //the reset button starts hidden, it simply reloads the page
@@ -53,15 +54,15 @@ function startGame() {
 	flash = 0;
 	turn = 1;
 
-	//update the turn counter in the html
+	//update the turn counter in the html to display the current level
 	turnCounter.innerHTML = 1;
 
-	//the game always starts with empty arrays that will be filled with the result of the for loop below and the player's guesses
+	//the game always starts with empty arrays that will be filled with the result of the for-loop and the player's guesses
 	engineSequence = [];
 	playerSequence = [];
 
 	//create an array of randomly assorted numbers from 1 to 4
-	for (let i = 0; i < 12; i++) {
+	for (let i = 0; i < 20; i++) {
 		engineSequence.push(Math.floor(Math.random() * 4) + 1);
 	}
 
@@ -71,17 +72,17 @@ function startGame() {
 }
 
 function gameTurn() {
-	//the engine hasn't played yet, so the flash will be less than turn; thus it will go on to else if...
+	//the engine hasn't played yet, so the flash will be less than turn; thus it will go on to if(engineTurn == true)...
 
-	//if this condition is true then it's the player's turn. since on is true, the lights' event listeners will now be active
+	//if this condition is true then it's the player's turn. since on = true, the lights' event listeners will now be active
 	if (flash == turn) {
 		engineTurn = false;
 		on = true;
-		clearInterval(intervalId);
 		clearAllLights();
+		clearInterval(intervalId);
 	} else if (engineTurn) {
 		//if the engine plays a flash, that flash will have the effect of a click
-		//a flash is added with each engine click to make progress toward reaching the number of the turn
+		//a flash is added with each engine click to make progress toward reaching the number of the current turn
 		setTimeout(() => {
 			if (engineSequence[flash] == 1) clickTopLeft();
 			else if (engineSequence[flash] == 2) clickTopRight();
@@ -94,12 +95,18 @@ function gameTurn() {
 }
 
 //the next four sequences affect the playerSequence
-//with each click, the light is compared with the engineArray with a corresponding light. the are numbered 1 to 4 clockwise from top left
 topLeft.addEventListener('click', (event) => {
 	if (on) {
+		//make it look/sound like clicked
 		clickTopLeft();
+
+		//push the clicked light into an array
 		playerSequence.push(1);
+
+		//check to see if that one was correct
 		checkCorrect();
+
+		//simulate mouseup by clearing all the lights
 		setTimeout(clearAllLights, 180);
 	}
 });
@@ -132,7 +139,7 @@ bottomLeft.addEventListener('click', (event) => {
 
 //flashes with sound-- for gameplay
 function clickTopLeft() {
-	console.log('clicked 1')
+	console.log('clicked 1');
 
 	topLeft.style.background = 'lightgreen';
 	topLeft.style.border = '8px solid rgb(201, 174, 174)';
@@ -140,7 +147,7 @@ function clickTopLeft() {
 	playTopLeft();
 }
 function clickTopRight() {
-	console.log('clicked 2')
+	console.log('clicked 2');
 
 	topRight.style.background = 'pink';
 	topRight.style.border = '8px solid rgb(201, 174, 174)';
@@ -148,7 +155,7 @@ function clickTopRight() {
 	playTopRight();
 }
 function clickBottomRight() {
-	console.log('clicked 3')
+	console.log('clicked 3');
 
 	bottomRight.style.background = 'lightblue';
 	bottomRight.style.border = '8px solid rgb(201, 174, 174)';
@@ -184,7 +191,7 @@ function clickBottomLeftMuted() {
 
 //this function checks for correct and follows up with a function depending on the condition
 function checkCorrect() {
-	//if the last clicked light by the player does not equal that index in the engineSequence, then the choice is deemed incorrect, more to next if statement
+	//if the last clicked light by the player does not equal that index in the engineSequence, then the choice is considered incorrect, move to next if statement
 	if (
 		playerSequence[playerSequence.length - 1] !==
 		engineSequence[playerSequence.length - 1]
@@ -192,28 +199,38 @@ function checkCorrect() {
 		correct = false;
 	}
 	//if the player is incorrect, all lights flash, and the display shows game over and your score
-	if (correct == false) {
+	if (!correct) {
+		//to express game over, all lights flash, the player cannot interact with the board anymore (because !on), and the display shows a game over message
 		flashAllLights();
+		on = false;
 		setTimeout(() => {
 			turnCounter.innerHTML = `GAME OVER. Your Score: ${turn}`;
 			clearAllLights();
 		}, 800);
 		setTimeout(playGameOverSound, 800);
-		on = false;
-		//however, if you are correct, you are passed to the next turn
-	} else if (turn == playerSequence.length && correct && !win && turn < 12) {
+
+		//however, if you are correct, you are passed to the next level (turn)
+		//this conditional statement makes the distinction between winning a round and winning the whole game
+	} else if (turn == playerSequence.length && correct && turn < 5) {
+		//if the player is correct, the turn counter increases by 1 to move on to the next round
 		turn++;
 		turnCounter.innerHTML = turn;
 
+		//flash must be 0 so the engine knows how many lights to click in this turn
 		flash = 0;
+
+		//player's array must also be clear. it is refilled with each turn
 		playerSequence = [];
 
+		//it is now the engine's turn
 		engineTurn = true;
 
+		//call the gameTurn function just like in startGame()
 		intervalId = setInterval(gameTurn, 800);
 
 		//if you win all turns, winGame function is called
-	} else if (playerSequence.length == 12 && correct) {
+	} else if (playerSequence.length == 5 && correct) {
+		win = true;
 		winGame();
 	}
 }
@@ -222,8 +239,8 @@ function checkCorrect() {
 function winGame() {
 	setTimeout(playWinGameSound, 800);
 
+	//the player can no longer interact with the game board and lights endlessly flash in a random order
 	on = false;
-	win = true;
 
 	intervalId = setInterval(flashAllLightsRandomly, 200);
 	turnCounter.innerHTML = 'CONGRATULATIONS! YOU WIN!';
@@ -307,11 +324,3 @@ function playBottomLeft() {
 	const bottomLeftSound = document.querySelector('.bottom-left-sound');
 	bottomLeftSound.play();
 }
-
-/*
-too add
--pastel color scheme
--more space and rounded edges for the 4 elements
--more exciting game display
--put a footer with your info
-*/
